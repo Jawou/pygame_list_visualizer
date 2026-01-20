61a6e2610a78211cb908bb97e962150812ade612
Jawou <88535234+Jawou@users.noreply.github.com>
2026-01-20 22:54:22 +0100

    Fix example usage to call update_screen method

--- list_visualizer.py
+++ list_visualizer.py
@@ -40,8 +40,8 @@ class numbers_displayer:
 
 
 #example usage:
-display= numbers_displayer(1000, 1000, [1, 2, 3], 1, 5, "example", [1, 2, 3])
-screen.update_screen()
+display = numbers_displayer(1000, 1000, [1, 2, 3], 1, 5, "example", [1, 2, 3])
+display.update_screen()
 
 while True:
   clock.tick(67)
