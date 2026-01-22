bb284c4a11d4591e66cf2f226b0ba44cc6f61a9e
Jawou <88535234+Jawou@users.noreply.github.com>
2026-01-22 01:18:52 +0100

    pain

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='pygame_list_visaulizer',
+  versions='0.1',
+  packages=find_packages(),
+  instal_requires=[
+    'pygame-ce'
+  ],
+)
