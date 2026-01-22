b7d5c8893e669dbd14f8cd0025681a98927ff815
Jawou <88535234+Jawou@users.noreply.github.com>
2026-01-22 01:47:23 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup, find_packages
 
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
-  name='pygame_list_visaulizer',
-  versions='0.1',
-  packages=find_packages(),
-  instal_requires=[
-    'pygame-ce'
-  ],
+    name='pygame_list_visaulizer',
+    version='0.0.1', 
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        'pygame-ce'
+    ],
 )
